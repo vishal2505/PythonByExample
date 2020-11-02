9e975b72332f354076b0a4cceacbcd4ea369e7f3
Vishal Mishra <vishalmishra@live.in>
2020-11-03 01:13:37 +0800

    Exception Handling and File Handling

---
+++ first_module.py
@@ -0,0 +1,7 @@
+print("This is going to executed always.")
+
+def main():
+    print("In first_module.py. Running Directly.")
+
+if __name__ == '__main__':
+    main()
---
+++ second_modle.py
@@ -0,0 +1,4 @@
+import first_module
+
+first_module.main()
+print("In second_module.py. Second module's name: {}".format(__name__))
